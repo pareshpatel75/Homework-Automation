2557229030b6563d8f4fb045ec8bf9e181aa4c3b
pareshpatel <pareshpatel75@gmail.com>
2022-04-10 20:54:47 +0100

    change file

--- IfElse.java
+++ IfElse.java
@@ -4,23 +4,23 @@ public class IfElse {
 
     // 18 is the Eligible for vote
     public static void main(String[] args) {
+        int num = 25;
 
-        int num1 = 18;
-
-        if (num1 >= 18) ;
+        if (num >= 18)
         {
-            System.out.println("candidate is eligible to vote");
+            System.out.println("candidate eligible for vote");
+
+        }else
+        {System.out.println("candidate not eligible for vote");
+
         }
-        if (num1 <18);
-        System.out.println("candidate is not eligible to vote");
-
-
-
-
-
     }
 
+
+
+
 }
+
 
 
 
