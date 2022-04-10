08ce6b31c5aab48f15efb51602dab571cc1a55fa
pareshpatel <pareshpatel75@gmail.com>
2022-04-10 20:58:29 +0100

    sunday chage

--- Unary.java
+++ Unary.java
@@ -4,12 +4,34 @@ public class Unary {
     //Unary operator Increment and Decrement operator
     public static void main(String[] args) {
 
-        int a = 23;
-        boolean b= true;
-        System.out.println(a++);
-        System.out.println(++a);
-        System.out.println(a--);
-        System.out.println(--a);
-        System.out.println(!b);
+        int a = 10;
+
+        System.out.println(a++);//print10
+        System.out.println(a++);//print11
+        System.out.println(a++);//print12
+        System.out.println(a++);//print13
+        System.out.println(a++);//print14
+        System.out.println(a++);//print15
+        System.out.println(a++);//print16
+        System.out.println(a++);//print17
+        System.out.println(a++);//print18
+        System.out.println(a++);//print19
+        System.out.println(a++);//print20
+
+        System.out.println("----------");
+
+        System.out.println(a--);//print21
+        System.out.println(a--);//print20
+        System.out.println(a--);//print19
+        System.out.println(a--);//print18
+        System.out.println(a--);//print17
+        System.out.println(a--);//print16
+        System.out.println(a--);//print15
+        System.out.println(a--);//print14
+        System.out.println(a--);//print13
+        System.out.println(a--);//print12
+        System.out.println(a--);//print11
+
+
     }
 }
